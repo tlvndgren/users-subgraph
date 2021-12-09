9273b1fd655d672afab572c3fc753db0a0f9d5d8
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-08 18:58:27 -0800

    Adjusting comments and fixing resolver.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// External Dependencies
 const fs = require("fs");
 const { ApolloServer, gql } = require("apollo-server");
 const { buildSubgraphSchema } = require("@apollo/federation")
+
+// Internal Dependencies
 const typeDefs = gql(fs.readFileSync("./users.graphql", 'utf8'));
 const users = require("./data/users.js");
 
+// Variable Definitions
 const port = process.env.PORT || 4003
 
 const resolvers = {
     User: {
-        __resolverReference(object) {
-            return users.find((user) => user.id === object.id);
+        __resolveReference(object) {
+            return users.find((user) => user.id === parseInt(object.id, 10));
         },
     },
     Query: {
@@ -22,10 +26,11 @@ const resolvers = {
     }
 };
 
+// Apollo Server Setup
 const server = new ApolloServer({
     schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
-  });
+});
   
-  server.listen({ port }).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
     console.log(`Users service ready at ${url}`);
-  });
+});
