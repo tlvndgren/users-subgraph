660bdd17c88a21cdfd963f43063569bc3b726e09
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-12 19:46:10 -0800

    Updating user schema.

--- data/users.js
+++ data/users.js
@@ -1,63 +1,94 @@
 const users = [
-    {
-      "id": 1,
-      "firstName": "Tashina",
-      "lastName": "Luettgen",
-      "userScore": 5
-    },
-    {
-      "id": 2,
-      "firstName": "Kendrick",
-      "lastName": "Adams",
-      "userScore": 5
-    },
-    {
-      "id": 3,
-      "firstName": "Ellyn",
-      "lastName": "Hills",
-      "userScore": 2
-    },
-    {
-      "id": 4,
-      "firstName": "Mazie",
-      "lastName": "Murazik",
-      "userScore": 5
-    },
-    {
-      "id": 5,
-      "firstName": "Isaiah",
-      "lastName": "Marks",
-      "userScore": 2
-    },
-    {
-      "id": 6,
-      "firstName": "Hilaria",
-      "lastName": "Heidenreich",
-      "userScore": 5
-    },
-    {
-      "id": 7,
-      "firstName": "Isidro",
-      "lastName": "Kertzmann",
-      "userScore": 1
-    },
-    {
-      "id": 8,
-      "firstName": "Sammie",
-      "lastName": "Klocko",
-      "userScore": 4
-    },
-    {
-      "id": 9,
-      "firstName": "Corrine",
-      "lastName": "Kuhic",
-      "userScore": 4
-    },
-    {
-      "id": 10,
-      "firstName": "Robbyn",
-      "lastName": "Jacobs",
-      "userScore": 2
-    }
+  {
+    "id": 1,
+    "firstName": "Dinorah",
+    "lastName": "Pfannerstill",
+    "userScore": 1,
+    "email": "chet.conn@gmail.com",
+    "username": "erin.haley",
+    "address": "Suite 662 525 McLaughlin Knolls, North Cliff, WV 01292-3889"
+  },
+  {
+    "id": 2,
+    "firstName": "Carroll",
+    "lastName": "Veum",
+    "userScore": 4,
+    "email": "chu_vandervort@yahoo.com",
+    "username": "cristobal.johnson",
+    "address": "Suite 276 8166 Deborah Unions, Johnstonburgh, MO 35236"
+  },
+  {
+    "id": 3,
+    "firstName": "Karima",
+    "lastName": "Fisher",
+    "userScore": 1,
+    "email": "nilsa.marks@gmail.com",
+    "username": "ileen_graham",
+    "address": "Apt. 992 82490 Bechtelar Station, South Judieport, WV 79991"
+  },
+  {
+    "id": 4,
+    "firstName": "Porsche",
+    "lastName": "Dickinson",
+    "userScore": 2,
+    "email": "sacha.jones@gmail.com",
+    "username": "wendy.fadel",
+    "address": "72431 Mosciski Hill, South Mervin, WA 81416-7213"
+  },
+  {
+    "id": 5,
+    "firstName": "Delores",
+    "lastName": "Carroll",
+    "userScore": 2,
+    "email": "mandi.kiehn@gmail.com",
+    "username": "veronique_hickle",
+    "address": "892 Greenholt Rue, Fidelfurt, NE 53840"
+  },
+  {
+    "id": 6,
+    "firstName": "Garth",
+    "lastName": "Stanton",
+    "userScore": 4,
+    "email": "delcie_rowe@gmail.com",
+    "username": "ignacio.kuhlman",
+    "address": "Suite 736 755 Murray Road, Mikechester, SC 35023-6262"
+  },
+  {
+    "id": 7,
+    "firstName": "Lester",
+    "lastName": "Langworth",
+    "userScore": 4,
+    "email": "lance_daugherty@gmail.com",
+    "username": "alda",
+    "address": "Suite 369 7555 Hessel Dam, Port Eugeneport, NH 96896-7108"
+  },
+  {
+    "id": 8,
+    "firstName": "Cyril",
+    "lastName": "Swift",
+    "userScore": 3,
+    "email": "sherlyn.mann@gmail.com",
+    "username": "kenneth_dach",
+    "address": "8588 Marilu Mountains, Barneyburgh, NM 92099"
+  },
+  {
+    "id": 9,
+    "firstName": "Mitch",
+    "lastName": "Feeney",
+    "userScore": 3,
+    "email": "zackary_hagenes@yahoo.com",
+    "username": "na.gerlach",
+    "address": "Suite 286 7021 Bertha Loaf, Lake Maricruzfurt, MO 38130"
+  },
+  {
+    "id": 10,
+    "firstName": "Marlin",
+    "lastName": "Wisozk",
+    "userScore": 5,
+    "email": "jonie@yahoo.com",
+    "username": "dorthea",
+    "address": "Suite 726 340 Reinger Bridge, South Martyberg, NH 40561"
+  }
 ];
+
 module.exports = users;
